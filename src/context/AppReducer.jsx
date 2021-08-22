3d9286bcb856e795c7bcecfd462e790a99a9e2b5
ucielsola <hello@ucielsola.com>
2021-08-21 22:21:33 -0300

    use context API

---
+++ src/context/AppReducer.jsx
@@ -0,0 +1,29 @@
+function AppReducer(state, action) {
+	switch (action.type) {
+		// case 'ADD_ITEM_TO_WATCHLIST':
+		// 	return {
+		// 		...state,
+		// 		watchlist: [action.payload, ...state.watchlist],
+		// 	};
+		// case 'REMOVE_ITEM_FROM_WATCHLIST':
+		// 	return {
+		// 		...state,
+		// 		watchlist: state.watchlist.filter((item) => item.id !== action.payload),
+		// 	};
+		// case 'ADD_ITEM_TO_WATCHED':
+		// 	return {
+		// 		...state,
+		// 		watchlist: state.watchlist.filter((item) => item.id !== action.payload),
+		// 		watched: [action.payload, ...state.watched],
+		// 	};
+		// case 'REMOVE_ITEM_FROM_WATCHED':
+		// 	return {
+		// 		...state,
+		// 		watched: state.watched.filter((item) => item.id !== action.payload),
+		// 	};
+		default:
+			return state;
+	}
+}
+
+export default AppReducer;
---
+++ src/context/GlobalState.jsx
@@ -0,0 +1,52 @@
+import React, { createContext, useReducer, useEffect } from 'react';
+import AppReducer from './AppReducer';
+
+// initial state
+const initialState = {
+	watchlist: localStorage.getItem('watchlist') ? JSON.parse(localStorage.getItem('watchlist')) : [],
+	watched: localStorage.getItem('watched') ? JSON.parse(localStorage.getItem('watched')) : [],
+};
+
+// create context
+export const GlobalContext = createContext(initialState);
+
+// provider component
+export const GlobalProvider = (props) => {
+	const [state, dispatch] = useReducer(AppReducer, initialState);
+
+	useEffect(() => {
+		localStorage.setItem('watchlist', JSON.stringify(state.watchlist));
+		localStorage.setItem('watched', JSON.stringify(state.watched));
+	}, [state]);
+
+	// actions
+	const addItemToWatchlist = (item) => {
+		dispatch({ type: 'ADD_ITEM_TO_WATCHLIST', payload: item });
+	};
+	const addItemToWatched = (item) => {
+		dispatch({ type: 'ADD_ITEM_TO_WATCHED', payload: item });
+	};
+
+	const removeItemFromWatchlist = (id) => {
+		dispatch({ type: 'REMOVE_ITEM_FROM_WATCHLIST', payload: id });
+		console.log('probando' + id);
+	};
+	const removeItemFromWatched = (id) => {
+		dispatch({ type: 'REMOVE_ITEM_FROM_WATCHed', payload: id });
+	};
+
+	return (
+		<GlobalContext.Provider
+			value={{
+				watchlist: state.watchlist,
+				watched: state.watched,
+				addItemToWatchlist,
+				addItemToWatched,
+				removeItemFromWatchlist,
+				removeItemFromWatched,
+			}}
+		>
+			{props.children}
+		</GlobalContext.Provider>
+	);
+};
