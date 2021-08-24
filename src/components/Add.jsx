8ff961a715208ecb86e357e5c1614e62c5840dce
ucielsola <hello@ucielsola.com>
2021-08-24 01:50:24 -0300

    switch fetch to axios

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,117 +1,60 @@
-import React, { useState, useContext } from 'react';
-import {ThemeContext}from "../context/ThemeContext"
+import React, { useState } from 'react';
+import axios from 'axios';
 import { FullCard } from './FullCard';
 import SearchGif from '../assets/search.webp';
 import './add.css';
 
 export const Add = () => {
-	// Theme Switcher
-	const { darkTheme, setDarkTheme } = useContext(ThemeContext);
-	let darkClass = darkTheme ? ' dark' : '';
-	// Add
 	const [query, setQuery] = useState('');
 	const [results, setResults] = useState([]);
-	const [page, setPage] = useState('');
-	const [totalPages, setTotalPages] = useState('');
 
-	const Search = (page) => (e) => {
+	const Search = (e) => {
 		e.preventDefault();
-		setPage(1);
-
 		setQuery(e.target.value);
-		fetch(
-			`https://api.themoviedb.org/3/search/multi?api_key=b8723cef7967276c30d0623e7338bcc4&language=en-US&page=1&include_adult=false&query=${e.target.value}&page=${page}`
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				if (!data.errors) {
-					console.log(data);
-					console.log('page is ' + page);
-					let arr = data.results.filter((item) => {
-						// Unifies name and title for Movies and TV Shows
-						if (item.media_type === 'tv') item.title = item.name;
-						// Filters Movie and TV Shows, excludes i.e. Persons
-						return item.media_type === 'movie' || item.media_type === 'tv';
-					});
 
-					setTotalPages(data.total_pages);
-					setResults(arr);
-				} else {
-					setResults([]);
-				}
+		axios
+			.get(
+				`https://api.themoviedb.org/3/search/multi?api_key=b8723cef7967276c30d0623e7338bcc4&language=en-US&page=1&include_adult=false&query=${e.target.value}&page=1`
+			)
+			.then((res) => {
+				const results = res.data.results.filter((item) => {
+					// Unifies name and title for Movies and TV Shows
+					if (item.media_type === 'tv') item.title = item.name;
+					// Filters Movie and TV Shows, excludes i.e. Persons
+					return item.media_type === 'movie' || item.media_type === 'tv';
+				});
+				setResults(results);
+			})
+			.catch((err) => {
+				console.log(err);
+				setResults([]);
 			});
-	};
-
-	const nextPage = (e) => {
-		e.preventDefault();
-		setPage(2);
-		console.log(page);
-		setQuery(e.target.value);
-		fetch(
-			`https://api.themoviedb.org/3/search/multi?api_key=b8723cef7967276c30d0623e7338bcc4&language=en-US&page=1&include_adult=false&query=${e.target.value}&page=${page}`
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				if (!data.errors) {
-					console.log(data);
-					console.log(page);
-					let arr = data.results.filter((item) => {
-						// Unifies name and title for Movies and TV Shows
-						if (item.media_type === 'tv') item.title = item.name;
-						// Filters Movie and TV Shows, excludes i.e. Persons
-						return item.media_type === 'movie' || item.media_type === 'tv';
-					});
-
-					setTotalPages(data.total_pages);
-					setResults(arr);
-				} else {
-					setResults([]);
-				}
-			});
-	};
-	const prevPage = (e) => {
-		e.preventDefault();
-		// let current = page;
-		// let previous = current - 1;
-		// setPage(previous);
-		// // Search(page);
-		// console.log(page);
 	};
 
 	return (
-		<div className={'add__container' + darkClass}>
-			<h2 className={'add__title' + darkClass}>Add Movies and TV Shows</h2>
-			<div className={'input__wrapper' + darkClass}>
+		<div className="add__container">
+			<h2 className="add__title">Add Movies and TV Shows</h2>
+			<div className="input__wrapper">
 				<input
 					type="text"
 					placeholder="Search for a Movie or TV Show"
 					value={query}
-					onChange={Search(page)}
+					onChange={Search}
 					// if (e.target.value.length >= 3)
 				/>
 			</div>
 
 			{results.length > 0 ? (
 				<React.Fragment>
-					<ul className={'add__results' + darkClass}>
+					<ul className="add__results">
 						{results.map((item) => (
-							<FullCard item={item} />
+							<FullCard item={item} key={item.id} />
 						))}
 					</ul>
-					{totalPages > 1 && (
-						<div className="button">
-							<button className={'add__prev-page' + darkClass} onClick={prevPage}>
-								Previous
-							</button>
-							<button className={'add__next-page' + darkClass} onClick={nextPage}>
-								Next
-							</button>
-						</div>
-					)}
 				</React.Fragment>
 			) : (
-				<div className={'add__gif-container' + darkClass}>
-					<img src={SearchGif} alt="Sarch GIF from Giphy.com" className={'add__gif' + darkClass} />
+				<div className="add__gif-container">
+					<img src={SearchGif} alt="Sarch GIF from Giphy.com" className="add__gif" />
 				</div>
 			)}
 		</div>
