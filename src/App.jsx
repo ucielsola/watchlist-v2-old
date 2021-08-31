870394f065c39a54a47c0c0db3ad571ba3e967ca
ucielsola <hello@ucielsola.com>
2021-08-31 01:02:40 -0300

    add back to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,35 @@ import React, { useState } from 'react';
 import { GlobalProvider } from './context/GlobalState';
 import { AuthProvider } from './context/AuthContext';
 import { SignUp } from './components/SignUp';
+import { LogIn } from './components/LogIn';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeContext } from './context/ThemeContext';
 import { Header } from './components/Header';
 import { Watchlist } from './components/Watchlist';
 import { Watched } from './components/Watched';
-import { ScrollToTop } from './components/ScrollToTop';
 import { Add } from './components/Add';
 import './App.css';
 
 function App() {
 	const [darkTheme, setDarkTheme] = useState(false);
 	return (
-		<AuthProvider>
-		<SignUp />
-		</AuthProvider>
+		<Router>
+			<AuthProvider>
+				<GlobalProvider>
+					<ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+						<Header />
+						<Switch>
+							<Route path="/signup" component={SignUp} />
+							<Route path="/login" component={LogIn} />
+							<Route exact path="/" component={Watchlist} />
+							<Route path="/watched" component={Watched} />
+							<Route path="/add" component={Add} />
+						</Switch>
+					</ThemeContext.Provider>
+				</GlobalProvider>
+			</AuthProvider>
+		</Router>
 	);
 }
 
 export default App;
-
-{
-	/* <GlobalProvider>
-				<ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
-					<Router>
-						<Header />
-
-						<main>
-							<ScrollToTop />
-							<Switch>
-								<Route exact path="/">
-									<Watchlist />
-								</Route>
-
-								<Route path="/watched">
-									<Watched />
-								</Route>
-
-								<Route path="/add">
-									<Add />
-								</Route>
-							</Switch>
-						</main>
-					</Router>
-				</ThemeContext.Provider>
-			</GlobalProvider> */
-}
--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -3,6 +3,8 @@ import { GlobalContext } from '../context/GlobalState';
 import { ThemeContext } from '../context/ThemeContext';
 import { PosterCard } from './PosterCard';
 import { EmptyPage } from './EmptyPage';
+import { ScrollToTop } from './ScrollToTop';
+
 import './watchlist.css';
 
 export const Watchlist = () => {
@@ -12,28 +14,32 @@ export const Watchlist = () => {
 	// Watchlists
 	const { watchlist } = useContext(GlobalContext);
 	return (
-		<div className={'watchlist__container' + darkClass}>
-			<h2 className={'watchlist__title' + darkClass}>Your Watch List</h2>
+		<React.Fragment>
+			<ScrollToTop />
 
-			{watchlist.length > 0 ? (
-				<React.Fragment>
-					<h3 className={'watchlist__subtitle' + darkClass}>
-						{watchlist.length === 1 ? '1 Movie / TVShow' : watchlist.length + ' Movies / TVShows'}
-					</h3>
-					<div className={'poster-card__container' + darkClass}>
-						{watchlist.map((item) => (
-							<PosterCard item={item} type="watchlist" key={item.id} />
-						))}
-					</div>
-				</React.Fragment>
-			) : (
-				<React.Fragment>
-					<h3 className={'watchlist__subtitle' + darkClass}>
-						Here you'll find all the Movies and TV Shows you want to watch!
-					</h3>
-					<EmptyPage />
-				</React.Fragment>
-			)}
-		</div>
+			<div className={'watchlist__container' + darkClass}>
+				<h2 className={'watchlist__title' + darkClass}>Your Watch List</h2>
+
+				{watchlist.length > 0 ? (
+					<React.Fragment>
+						<h3 className={'watchlist__subtitle' + darkClass}>
+							{watchlist.length === 1 ? '1 Movie / TVShow' : watchlist.length + ' Movies / TVShows'}
+						</h3>
+						<div className={'poster-card__container' + darkClass}>
+							{watchlist.map((item) => (
+								<PosterCard item={item} type="watchlist" key={item.id} />
+							))}
+						</div>
+					</React.Fragment>
+				) : (
+					<React.Fragment>
+						<h3 className={'watchlist__subtitle' + darkClass}>
+							Here you'll find all the Movies and TV Shows you want to watch!
+						</h3>
+						<EmptyPage />
+					</React.Fragment>
+				)}
+			</div>
+		</React.Fragment>
 	);
 };
